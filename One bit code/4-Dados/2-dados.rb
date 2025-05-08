7b8ae99acc09509a051483f6abbcb2442252d455
bowie <kiiim.martins@gmail.com>
2025-05-08 13:11:10 -0300

    Create 2-dados.rb

---
+++ One bit code/4-Dados/2-dados.rb
@@ -0,0 +1,8 @@
+=begin
+ Continuação sobre banco de dados 
+
+
+              INSERIR DADOS NAS TABELAS CRIADAS NO BANCO DE DADOS 
+Para inserir dados nas tabelas criadas usamos o comando INSERT INTO + nome da tabela + (nomes da coluna a ser prenchida) + VALUES + (infos das colunas escolhidas) + ;
+
+=end
