c01facda41785ce6d57d9dcafb8080456b004602
bowie <kiiim.martins@gmail.com>
2025-05-08 13:18:42 -0300

    Update 2-dados.rb

--- One bit code/4-Dados/2-dados.rb
+++ One bit code/4-Dados/2-dados.rb
@@ -3,6 +3,14 @@
 
 
               INSERIR DADOS NAS TABELAS CRIADAS NO BANCO DE DADOS 
-Para inserir dados nas tabelas criadas usamos o comando INSERT INTO + nome da tabela + (nomes da coluna a ser prenchida) + VALUES + (infos das colunas escolhidas) + ;
+Para inserir dados nas tabelas criadas usamos o comando INSERT INTO + nome da tabela + (nomes da coluna a ser prenchida) + VALUES + (infos das colunas escolhidas) + ; e por fim executar a querry.
+
+EX:
+INSERT INTO clients (name, phone, adress, created_at) 
+VALUES ('Kim', '41 123456789', 'Rua do Medo', '2025-05-08');
+
+No PgADMIN se tiver sido executada com sucesso ela retornará
+INSERT 0 1
+Query returned successfully in 168 msec.
 
 =end
