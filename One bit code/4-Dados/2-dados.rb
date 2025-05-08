da9d575932e6b2d5f80dde76a8071de61db46bf8
bowie <kiiim.martins@gmail.com>
2025-05-08 13:24:32 -0300

    info string
    add details about strings in value on database

--- One bit code/4-Dados/2-dados.rb
+++ One bit code/4-Dados/2-dados.rb
@@ -5,6 +5,8 @@
               INSERIR DADOS NAS TABELAS CRIADAS NO BANCO DE DADOS 
 Para inserir dados nas tabelas criadas usamos o comando INSERT INTO + nome da tabela + (nomes da coluna a ser prenchida) + VALUES + (infos das colunas escolhidas) + ; e por fim executar a querry.
 
+******** STRINGS NO VALUE TEM QUE ESTAR COM ASPAS SIMPLES *********
+
 EX:
 INSERT INTO clients (name, phone, adress, created_at) 
 VALUES ('Kim', '41 123456789', 'Rua do Medo', '2025-05-08');
