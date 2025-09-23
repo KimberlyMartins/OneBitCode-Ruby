4f1d32e70af5024089e5e0a83ee966ff71082548
bowie <kiiim.martins@gmail.com>
2025-09-23 16:22:05 -0300

    exercise2
    started to solve the exercise

--- One bit code/4-Dados/exercicio2.rb
+++ One bit code/4-Dados/exercicio2.rb
@@ -42,7 +42,7 @@ Além disso, crie também as seguintes consultas:
 - A média de avaliação entre os filmes de até 2 horas e a média de avaliação dos filmes de mais de 2 horas (em colunas separadas).
 - Os nomes, anos de lançamento e avaliações dos filmes ordenados pelo lucro obtido, além do próprio lucro obtido (considere lucro como bilheteria - custo).
 =end
-
+                                   PARTE 1
 =begin
 Tentativas de resolver passo a passo
 - No pgAdmin clica em database - create database 
@@ -76,7 +76,8 @@ production_cost DECIMAL
 
 Em decimal, precisa dizer quantos números antes e depois da vírgula ex: DECIMAL(15,2)
 =end 
-RESOLUÇÃO 
+RESOLUÇÃO PARTE 1
+
 CREATE TABLE IF NOT EXISTS movies (
 id SERIAL PRIMARY KEY,
 title VARCHAR(300) NOT NULL,
@@ -97,4 +98,83 @@ Query returned successfully in 289 msec.
  id | title | director | release_year | genre | duration | rating | box_office | production_cost
 ----+-------+----------+--------------+-------+----------+--------+------------+-----------------
 (0 linha) 
+=end
+                                   PARTE 2
+=begin
+Tentativa 1
+CREATE TABLE IF NOT EXISTS series (
+id SERIAL PRIMARY KEY,
+title VARCHAR(300) NOT NULL,
+creator VARCHAR(250) NOT NULL,
+release_year INT,
+genre VARCHAR(100),
+seasons INT,
+episodes INT,
+rating DECIMAL(3,2),
+channel VARCHAR(100),
+situation VARCHAR(100)
+);
+=end
+
+RESOLUÇÃO PARTE 2
+
+CREATE TABLE IF NOT EXISTS tv_series (
+id SERIAL PRIMARY KEY,
+title VARCHAR(300) NOT NULL,
+creator VARCHAR(255),
+release_year INT,
+genre VARCHAR(100),
+seasons INT,
+episodes INT,
+rating DECIMAL(3,2),
+network VARCHAR(100),
+status VARCHAR(100)
+);
+
+=begin
+CREATE TABLE
+
+Query returned successfully in 270 msec.
+ SELECT * FROM tv_series;
+ id | title | creator | release_year | genre | seasons | episodes | rating | network | status
+----+-------+---------+--------------+-------+---------+----------+--------+---------+--------
+(0 linha)
+=end
+PARTE 3 
+RESOLUÇÃO
+
+INSERT INTO movies(title, director, release_year, genre, duration, rating, box_office, production_cost)
+VALUES
+('Mad Max: Fury Road', 'George Miller', 2015, 'Action', 120, 8.1, 375200000.00, 150000000.00),
+('Star Wars', 'George Lucas', 1977, 'Sci-Fi', 121, 8.6, 775398007.00, 11000000.00),
+('Super Mario Bros', 'Aaron Horvath, Michael Jelenic', 2023, 'Animation', 92, 7.3, 1300000000.00, 100000000.00),
+('Pride and Prejudice', 'Joe Wright', 2005, 'Romance', 129, 7.8, 121147947.00, 28000000.00),
+('Back to the Future', 'Robert Zemeckis', 1985, 'Sci-Fi', 116, 8.5, 381109762.00, 19000000.00),
+('The Godfather', 'Francis Ford Coppola', 1972, 'Crime', 175, 9.2, 246120974.00, 6000000.00),
+('The Lord of the Rings: The Return of the King', 'Peter Jackson', 2003, 'Fantasy', 201, 9.0, 1146030912.00, 94000000.00),
+('Treasure Planet', 'Ron Clements, John Musker', 2002, 'Animation', 95, 7.2, 109578115.00, 140000000.00),
+('Jurassic Park', 'Steven Spielberg', 1993, 'Adventure', 127, 8.1, 1043580597.00, 63000000.00),
+('About Time', 'Richard Curtis', 2013, 'Romance', 123, 7.8, 87100000.00, 12000000.00),
+('Transformers', 'Michael Bay', 2007, 'Action', 144, 7.0, 709709780.00, 150000000.00);
+
+=begin
+INSERT 0 11
+
+Query returned successfully in 171 msec.
+
+SELECT * FROM movies;
+ id |                     title                     |            director            | release_year |   genre   | duration | rating |  box_office   | production_cost
+----+-----------------------------------------------+--------------------------------+--------------+-----------+----------+--------+---------------+-----------------
+  1 | Mad Max: Fury Road                            | George Miller                  |         2015 | Action    |      120 |   8.10 |  375200000.00 |    150000000.00
+  2 | Star Wars                                     | George Lucas                   |         1977 | Sci-Fi    |      121 |   8.60 |  775398007.00 |     11000000.00
+  3 | Super Mario Bros                              | Aaron Horvath, Michael Jelenic |         2023 | Animation |       92 |   7.30 | 1300000000.00 |    100000000.00
+  4 | Pride and Prejudice                           | Joe Wright                     |         2005 | Romance   |      129 |   7.80 |  121147947.00 |     28000000.00
+  5 | Back to the Future                            | Robert Zemeckis                |         1985 | Sci-Fi    |      116 |   8.50 |  381109762.00 |     19000000.00
+  6 | The Godfather                                 | Francis Ford Coppola           |         1972 | Crime     |      175 |   9.20 |  246120974.00 |      6000000.00
+  7 | The Lord of the Rings: The Return of the King | Peter Jackson                  |         2003 | Fantasy   |      201 |   9.00 | 1146030912.00 |     94000000.00
+  8 | Treasure Planet                               | Ron Clements, John Musker      |         2002 | Animation |       95 |   7.20 |  109578115.00 |    140000000.00
+  9 | Jurassic Park                                 | Steven Spielberg               |         1993 | Adventure |      127 |   8.10 | 1043580597.00 |     63000000.00
+ 10 | About Time                                    | Richard Curtis                 |         2013 | Romance   |      123 |   7.80 |   87100000.00 |     12000000.00
+ 11 | Transformers                                  | Michael Bay                    |         2007 | Action    |      144 |   7.00 |  709709780.00 |    150000000.00
+(11 linhas)
 =end
