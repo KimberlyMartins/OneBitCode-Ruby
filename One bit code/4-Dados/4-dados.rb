f7b382bbb244031e8c5502e8acea08d4271b4ca6
bowie <kiiim.martins@gmail.com>
2025-10-22 13:56:18 -0300

    Update 4-dados.rb

--- One bit code/4-Dados/4-dados.rb
+++ One bit code/4-Dados/4-dados.rb
@@ -1,2 +1,3 @@
                            BACKUP E RESTAURAÇÃO
 O BACKUP e a RESTAURAÇÃO são imprescindíveis para a segurança do nosso banco de dados.
+No PostgreSQL, esses processos podem ser realizados usando os utilitários de linha de comando pg_dump e pg_restore.
