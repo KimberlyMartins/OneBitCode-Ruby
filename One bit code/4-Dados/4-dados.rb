3b934a1871a2b08f2e8fd460d782c3c1056d1158
bowie <kiiim.martins@gmail.com>
2025-10-22 11:18:21 -0300

    backup
    info

--- One bit code/4-Dados/4-dados.rb
+++ One bit code/4-Dados/4-dados.rb
@@ -1,1 +1,2 @@
-                         Trabalhando com BACKUP e RESTAURAÇÃO 
+                           BACKUP E RESTAURAÇÃO
+O BACKUP e a RESTAURAÇÃO são imprescindíveis para a segurança do nosso banco de dados.
