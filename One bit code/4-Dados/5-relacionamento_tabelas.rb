e3a0cc9162b891b549cc5a3095d5894b2d125354
bowie <kiiim.martins@gmail.com>
2025-11-07 14:40:19 -0300

    Create 5-relacionamento_tabelas.rb

---
+++ One bit code/4-Dados/5-relacionamento_tabelas.rb
@@ -0,0 +1,9 @@
+                    RELACIONAMENTO ENTRE TABELAS
+Relacionamentos também são chamados de associações, e são formas de vincularmos os dados de uma tabela aos dados de outra tabela.
+=begin
+Exemplo
+Temos 2 tabelas: Clientes e Endereços, o relacionamento permite que associemos uma linha
+da tabela de usuários a um endereço específico
+=end
+
+Existem três tipos de relacionamento,  um para um (1:1), um para muitos (1:n), e muitos para muitos (n:n).
