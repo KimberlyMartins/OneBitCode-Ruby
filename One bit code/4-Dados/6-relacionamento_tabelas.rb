cc55b7bdce7410115cba4eee8374264e295d3df1
bowie <kiiim.martins@gmail.com>
2026-02-19 15:45:30 -0300

    join
    how use join in database postgres

---
+++ One bit code/4-Dados/6-relacionamento_tabelas.rb
@@ -0,0 +1,53 @@
+                                   JOIN
+Vamos conhecer um conceito importante no relacionamento entre tabelas, o JOIN.
+Ele serve para conseguir obter dados de tabelas relacionadas em uma única consulta. Vamos experimentar obter os dados dos funcionários junto com seus respectivos endereços:
+Ele serve para conseguir obter dados de tabelas relacionadas em uma única consulta. Vamos experimentar obter os dados dos funcionários junto com seus respectivos endereços:
+
+=begin
+aula_relacionamento_tabelas=# select * from employees JOIN addresses ON employees.id = addresses.employee_id;;
+ id |     name     |   phone   | departament_id | id | street | number | complement |    city    | employee_id
+----+--------------+-----------+----------------+----+--------+--------+------------+------------+-------------
+  1 | Clark Kent   | 1111-1111 |              1 |  1 | Rua A  |        |            | Metropolis |           1
+  2 | Bruce Wayne  | 2222-2222 |              2 |  2 | Rua B  |        |            | Gotham     |           2
+  3 | Diana Prince | 3333-3333 |              3 |  3 | Rua C  |        |            | Themyscira |           3
+(3 linhas)
+=end
+
+O mesmo vale para o relacionamento um-para-muitos:Obs.: repare que o cuidado na consulta com qual é a tabela consultada e qual é a tabela da junção é apenas por uma questão de organização, ambos os resultados são os mesmos, mas organizados de forma diferente.
+
+=begin
+aula_relacionamento_tabelas=# SELECT * FROM employees JOIN departaments ON employees.departament_id = departaments.id;
+ id |     name     |   phone   | departament_id | id |     name
+----+--------------+-----------+----------------+----+--------------
+  1 | Clark Kent   | 1111-1111 |              1 |  1 | Imprensa
+  2 | Bruce Wayne  | 2222-2222 |              2 |  2 | InvestigaþÒo
+  3 | Diana Prince | 3333-3333 |              3 |  3 | Diplomacia
+  4 | John Jones   | 4444-4444 |              2 |  2 | InvestigaþÒo
+(4 linhas)
+=end
+
+
+-- Ou podemos obter os departamentos com uma junção com a tabela de funcionários
+
+=begin
+aula_relacionamento_tabelas=# SELECT * FROM departaments JOIN employees ON employees.departament_id = departaments.id;
+ id |     name     | id |     name     |   phone   | departament_id
+----+--------------+----+--------------+-----------+----------------
+  1 | Imprensa     |  1 | Clark Kent   | 1111-1111 |              1
+  2 | InvestigaþÒo |  2 | Bruce Wayne  | 2222-2222 |              2
+  3 | Diplomacia   |  3 | Diana Prince | 3333-3333 |              3
+  2 | InvestigaþÒo |  4 | John Jones   | 4444-4444 |              2
+(4 linhas)
+=end
+
+Por fim, também podemos customizar nossa consulta ao identificar as tabelas e colunas da seguinte forma:
+=begin
+aula_relacionamento_tabelas=# SELECT employees.id AS ID,employees.name AS Funcionário,employees.phone AS Telefone,departaments.name AS Departamento FROM employees JOIN departaments ON employees.departament_id = departaments.id;
+ id | funcionário  | telefone  | departamento
+----+--------------+-----------+--------------
+  1 | Clark Kent   | 1111-1111 | Imprensa
+  2 | Bruce Wayne  | 2222-2222 | InvestigaþÒo
+  3 | Diana Prince | 3333-3333 | Diplomacia
+  4 | John Jones   | 4444-4444 | InvestigaþÒo
+(4 linhas)
+=end
