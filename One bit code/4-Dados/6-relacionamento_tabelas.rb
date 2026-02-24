1dcf65c3a209b3142b17c53fd5f0874a63c54e75
bowie <kiiim.martins@gmail.com>
2026-02-24 15:06:58 -0300

    Update 6-relacionamento_tabelas.rb

--- One bit code/4-Dados/6-relacionamento_tabelas.rb
+++ One bit code/4-Dados/6-relacionamento_tabelas.rb
@@ -220,3 +220,74 @@ RESUMINDO
 - SET NULL → deixa filhos órfãos com NULL.
 - SET DEFAULT → redireciona filhos para valor padrão.
 - RESTRICT / NO ACTION → bloqueia exclusão/atualização se houver filhos.
+
+
+               EXEMPLOS INTEGRIDADE REFERENCIAL
+- Crie a tabela de clientes
+
+CREATE TABLE customers (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	phone VARCHAR(100) UNIQUE NOT NULL
+);
+
+- Insira dados na tabela de clientes
+INSERT INTO customers (name, phone) VALUES
+	('Clark Kent', 'clark@email.com'),
+	('Bruce Wayne', 'bruce@email.com'),
+	('Diana Prince', 'diana@email.com');
+
+- Crie a tabela de pedidos
+CREATE TABLE orders (
+	id SERIAL PRIMARY KEY,
+	total DECIMAL(10, 2),
+	customer_id INT,
+	FOREIGN KEY (customer_id) REFERENCES customers (id)
+);
+
+- Insira dados na tabela de pedidos
+INSERT INTO orders (total, customer_id) VALUES
+	(100.00, 1),
+	(240.00, 2),
+	(200.00, 1),
+	(420.00, 3),
+	(700.00, 2);
+
+DELETE FROM customers WHERE id = 1; # Ao tentar apagar, lançará um erro de violação de constraint
+Como não definimos uma cláusula, pelo padrão sql ele vai bloquear a exclusão para manter a integridade referencial!
+
+Então recriaremos a tabela com suas constraints definidas para poder fazer as devidas alterações.
+
+- Exclua a tabela para recriá-la com a nova constraint
+DROP TABLE orders; 
+
+CREATE TABLE orders (
+	id SERIAL PRIMARY KEY,
+	total DECIMAL(10, 2),
+	customer_id INT,
+	FOREIGN KEY (customer_id) REFERENCES customers (id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+-- Reinserir dados na tabela de pedidos
+INSERT INTO orders (total, customer_id) VALUES
+	(100.00, 1),
+	(240.00, 2),
+	(200.00, 1),
+	(420.00, 3),
+	(700.00, 2);
+
+Agora sim podemos experimentar excluir e atualizar o ID de alguns registros para ver o resultados 
+
+Exclua um cliente e observe o efeito nos pedidos
+DELETE FROM customers WHERE id = 1;
+
+Verifique os pedidos restantes
+SELECT * FROM orders JOIN customers ON customers.id = orders.customer_id;
+
+Atualize um cliente e observe o efeito nos pedidos
+UPDATE customers SET id = 40 WHERE id = 2;
+
+Verifique os pedidos atualizados
+SELECT * FROM orders JOIN customers ON customers.id = orders.customer_id;
