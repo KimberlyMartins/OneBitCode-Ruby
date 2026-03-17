d506a9fcfdef00eeb93ea36ef42e9314e318c38f
bowie <kiiim.martins@gmail.com>
2026-03-17 14:58:25 -0300

    Update 9-atual_dados_mongo.rb

--- One bit code/4-BD/9-atual_dados_mongo.rb
+++ One bit code/4-BD/9-atual_dados_mongo.rb
@@ -5,3 +5,13 @@ require 'mongo'
 client = Mongo::Client.new(['localhost:27017'], :database => 'ecommerce')
 
 #Atualização de um ou mais dados  (update_one ou update_many)
+result = client[:products].update_one({name: 'Smartphone'}, {
+  "$set" => {category: {name: 'Mobile devices'}}
+})
+
+#Verificação do resultado de atualização
+if result.modified_count > 0
+  puts "Dados atualizados com sucesso."
+else
+  puts "Nenhum documento foi atualizado."
+end
