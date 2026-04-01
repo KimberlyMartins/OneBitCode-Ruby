f946754141409e35681eadec22650daceb2e6d81
bowie <kiiim.martins@gmail.com>
2026-04-01 14:37:39 -0300

    Update exercicio4.rb

--- One bit code/4-Dados/exercicio4.rb
+++ One bit code/4-Dados/exercicio4.rb
@@ -18,3 +18,49 @@ cadastro de autores: nome, biografia, data de nascimento e lista de livros publi
 leitores: nome -  email - idade e o gênero opcionais
 
 resenha: leitores e livros q leram - nota e comentario detalhado
+
+CREATE TABLE IF NOT EXISTS authors (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  biography TEXT,
+  birthday DATE
+  );
+
+CREATE TABLE IF NOT EXISTS genres (
+   id SERIAL PRIMARY KEY,
+   title VARCHAR(255) NOT NULL
+   );
+   
+CREATE TABLE IF NOT EXISTS books (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  genre_id INT NOT NULL REFERENCES genres(id),
+  publication_date DATE,
+  isbn VARCHAR(255),
+  summary TEXT
+  );
+
+CREATE TABLE IF NOT EXISTS book_authors (
+  book_id INT,
+  author_id INT,
+  PRIMARY KEY (book_id, author_id),
+  FOREIGN KEY (book_id) REFERENCES books(id),
+  FOREIGN KEY (author_id) REFERENCES authors(id)
+  );
+  
+  CREATE TABLE IF NOT EXISTS readers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    gender CHAR(1),
+    birthday DATE
+    );
+    
+ CREATE TABLE IF NOT EXISTS reviews (
+   id SERIAL PRIMARY KEY,
+   rating INT,
+   comment TEXT,
+   book_id INT NOT NULL REFERENCES books(id),
+   reader_id INT NOT NULL REFERENCES readers(id)
+   );
