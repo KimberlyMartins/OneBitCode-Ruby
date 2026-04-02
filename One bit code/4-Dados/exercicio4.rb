a55090a8f4603bfdcff6d6b31a91699f734ac288
bowie <kiiim.martins@gmail.com>
2026-04-02 10:21:06 -0300

    Update exercicio4.rb

--- One bit code/4-Dados/exercicio4.rb
+++ One bit code/4-Dados/exercicio4.rb
@@ -138,4 +138,49 @@ INSERT INTO book_authors VALUES
 RETORNANDO O ERRO 
 inserção ou atualização em tabela "books" viola restrição de chave estrangeira "books_genre_id_fkey"
 =end
+
+CREATE TABLE IF NOT EXISTS authors (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  biography TEXT,
+  birthday DATE
+  );
+
+CREATE TABLE IF NOT EXISTS genres (
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(255) NOT NULL
+   );
    
+CREATE TABLE IF NOT EXISTS books (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  genre_id INT NOT NULL REFERENCES genres(id),
+  publication_date DATE,
+  isbn VARCHAR(255),
+  summary TEXT
+  );
+
+CREATE TABLE IF NOT EXISTS book_authors (
+  book_id INT,
+  author_id INT,
+  PRIMARY KEY (book_id, author_id),
+  FOREIGN KEY (book_id) REFERENCES books(id),
+  FOREIGN KEY (author_id) REFERENCES authors(id)
+  );
+  
+  CREATE TABLE IF NOT EXISTS readers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    gender CHAR(1),
+    birthday DATE
+    );
+    
+ CREATE TABLE IF NOT EXISTS reviews (
+   id SERIAL PRIMARY KEY,
+   book_id INT NOT NULL REFERENCES books(id),
+   reader_id INT NOT NULL REFERENCES readers(id),
+   rating INT CHECK (rating >= 1 AND rating <= 5),
+   comment TEXT
+   );
