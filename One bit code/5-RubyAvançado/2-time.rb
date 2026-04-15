6db396c725c2c8ff4222ae15c1bd79fb6454141e
bowie <kiiim.martins@gmail.com>
2026-04-15 10:33:46 -0300

    Update 2-time.rb

--- One bit code/5-RubyAvançado/2-time.rb
+++ One bit code/5-RubyAvançado/2-time.rb
@@ -1,3 +1,8 @@
+=begin
+                        Time
+Diferente do Date, que foca apenas no calendário, o objeto Time no Ruby é usado para representar momentos específicos incluindo horas, minutos, segundos e microsegundos. No Ruby moderno, ele já vem carregado por padrão, então você geralmente não precisa de require.
+=end
+
 puts Time.new
 puts Time.now
 
