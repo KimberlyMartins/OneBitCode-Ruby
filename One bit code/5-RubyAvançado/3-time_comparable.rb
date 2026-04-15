135d49f144213ae0201b272899a67883d4c994e3
bowie <kiiim.martins@gmail.com>
2026-04-15 13:39:42 -0300

    Update 3-time_comparable.rb

--- One bit code/5-RubyAvançado/3-time_comparable.rb
+++ One bit code/5-RubyAvançado/3-time_comparable.rb
@@ -1,4 +1,10 @@
-birthday = Time.now(2005, 1 , 18)
+birthday = Time.new(2005, 1 , 18)
 summer = Time.new(2020, 5, 21)
 winter = Time.new(2022, 3, 10)
 independence_day = Time.new(1822, 9 , 7)
+
+puts birthday # 2005-01-18 00:00:00 -0300
+
+puts birthday > independence_day # true
+puts summer >= winter # false
+puts independence_day > winter # false
