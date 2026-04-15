bec2addbb55e9cb9e1b51a91373c0bb3972df1fe
bowie <kiiim.martins@gmail.com>
2026-04-15 11:23:11 -0300

    Update 2-time.rb

--- One bit code/5-RubyAvançado/2-time.rb
+++ One bit code/5-RubyAvançado/2-time.rb
@@ -1,5 +1,5 @@
 =begin
-                        Time
+                                     Time
 Diferente do Date, que foca apenas no calendário, o objeto Time no Ruby é usado para representar momentos específicos incluindo horas, minutos, segundos e microsegundos. No Ruby moderno, ele já vem carregado por padrão, então você geralmente não precisa de require.
 =end
 
@@ -14,3 +14,9 @@ em outro dia buscou outra data
 2026-04-15 11:10:20 -0300
 2026-04-15 11:10:20 -0300
 =end
+
+#Buscando time sem especificar as informações completas obtemos a busca default
+puts Time.new(2023) # 2023-01-01 00:00:00 -0300 puxa a data e horário default(padrão)
+
+#Podemos buscar também especificando todas as infos (ano, mês, dia, hora, minuto e segundo)
+puts Time.new(2023, 10, 5, 18, 43, 51) # 2023-10-05 18:43:51 -0300
