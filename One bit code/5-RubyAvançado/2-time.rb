08c55121e3ca5b80babcc1821f4e6e1b47c4f229
bowie <kiiim.martins@gmail.com>
2026-04-15 11:26:44 -0300

    Update 2-time.rb

--- One bit code/5-RubyAvançado/2-time.rb
+++ One bit code/5-RubyAvançado/2-time.rb
@@ -15,8 +15,18 @@ em outro dia buscou outra data
 2026-04-15 11:10:20 -0300
 =end
 
-#Buscando time sem especificar as informações completas obtemos a busca default
+# Buscando time sem especificar as informações completas obtemos a busca default
 puts Time.new(2023) # 2023-01-01 00:00:00 -0300 puxa a data e horário default(padrão)
 
-#Podemos buscar também especificando todas as infos (ano, mês, dia, hora, minuto e segundo)
+# Podemos buscar também especificando todas as infos (ano, mês, dia, hora, minuto e segundo)
 puts Time.new(2023, 10, 5, 18, 43, 51) # 2023-10-05 18:43:51 -0300
+
+# Criando uma váriável assim como no date, podemos recuperar dados como dia, hora, ano...
+some_time = Time.new(2023, 10, 5, 18, 43, 51)
+puts some_time.year
+puts some_time.month
+puts some_time.day
+
+puts some_time.hour
+puts some_time.min
+puts some_time.sec
