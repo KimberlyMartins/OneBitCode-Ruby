ecc7aac3ab9e2ad1d9afd8b54eef9babc9ce45c6
bowie <kiiim.martins@gmail.com>
2026-05-06 11:29:54 -0300

    Update exer1.rb

--- One bit code/5-RubyAvançado/exer1.rb
+++ One bit code/5-RubyAvançado/exer1.rb
@@ -2,4 +2,10 @@
 ​Você foi contratado por uma empresa de logística para ajudar a calcular o tempo estimado de entrega de pacotes. A empresa deseja uma função que receba a data de envio e o tempo estimado de entrega em dias úteis e retorne a data estimada de entrega, levando em consideração apenas os dias úteis (segunda a sexta-feira).
 
 Escreva uma função chamada calcular_data_entrega que recebe dois parâmetros: data_envio (no formato "YYYY-MM-DD") e tempo_entrega_dias (um inteiro representando os dias úteis necessários para a entrega). A função deve retornar a data estimada de entrega no formato "YYYY-MM-DD", levando em consideração os dias úteis.
+
+Montando a resolução 
+calcular_data_entrega >  data_envio (no formato "YYYY-MM-DD") e tempo_entrega_dias
+retornando  data estimada "YYYY-MM-DD" contando somente dias uteis 
 =end
+calcular_data_entrega >  data_envio (no formato "YYYY-MM-DD") e tempo_entrega_dias
+retornando  data estimada "YYYY-MM-DD" contando somente dias uteis 
