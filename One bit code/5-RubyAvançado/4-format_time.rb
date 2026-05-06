68daeb4f1e15b1020154ed25f58231242f54989c
bowie <kiiim.martins@gmail.com>
2026-05-06 10:47:57 -0300

    Update 4-format_time.rb

--- One bit code/5-RubyAvançado/4-format_time.rb
+++ One bit code/5-RubyAvançado/4-format_time.rb
@@ -1,14 +1,17 @@
 =begin
 Tabela de referências da formatação de tempo
 
-%b - Abbreviated month name ("Jan")
-%B - Full month name ("January")
-%d - Day of the month (1..31)
-%j - Day of the year (1..366); so-called "Julian date"
-%m - Month as a number (1..12)
-%w - Day of the week as a number (0..6)
+%b - Abbreviated month name ("Jan") - nome do mês abreviado
+%B - Full month name ("January") - nome do mês inteiro
+%d - Day of the month (1..31) - dia do mês
+%j - Day of the year (1..366); so-called "Julian date" - dia do ano
+%m - Month as a number (1..12) - mês como um número
+%w - Day of the week as a number (0..6) - dia da semana como número
 %x - Preferred representation for date (no time)
-%y - Two-digit year (no century)
-%Y - Four-digit year
+%y - Two-digit year (no century) - Ano em dois dígitos
+%Y - Four-digit year - Ano em quatro dígitos
 
 =end
+
+someday = Time.new(2025, 3, 31)
+
