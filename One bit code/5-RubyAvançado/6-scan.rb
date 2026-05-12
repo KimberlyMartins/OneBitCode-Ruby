71581b136fb069d5c90b21083930d64e50113b51
bowie <kiiim.martins@gmail.com>
2026-05-12 16:37:33 -0300

    Update 6-scan.rb

--- One bit code/5-RubyAvançado/6-scan.rb
+++ One bit code/5-RubyAvançado/6-scan.rb
@@ -9,3 +9,8 @@ puts results.length # 2
 
 puts "O padrão Ruby foi encontrado #{results.length} vezes na string" # O padrão Ruby foi encontrado 2 vezes na string
 puts "As ocorrências encontradas foram #{results.join(',')}" # As ocorrências encontradas foram Ruby,Ruby
+
+#Extrair todos os dígitos da string com scan
+string2 = "A OpenAI lançou o GPT-3 em 2020 e o GPT-4 em 2022."
+numbers = string2.scan(/\d+/)
+puts "Os números encontrados na string são: #{numbers.join(',')}" # Os números encontrados na string são: 3,2020,4,2022
