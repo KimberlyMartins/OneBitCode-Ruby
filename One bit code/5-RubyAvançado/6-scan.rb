06bca1188a3fa8f38b7e88e9600b545946fe571c
bowie <kiiim.martins@gmail.com>
2026-05-12 16:46:25 -0300

    Update 6-scan.rb

--- One bit code/5-RubyAvançado/6-scan.rb
+++ One bit code/5-RubyAvançado/6-scan.rb
@@ -1,3 +1,10 @@
+=begin
+O método scan em Ruby serve para buscar padrões dentro de uma string usando expressões regulares ou substrings simples. Ele retorna um array com todas as ocorrências encontradas.
+Você passa uma expressão regular ou uma string como argumento.
+O método percorre a string original e coleta todas as correspondências.
+O resultado é sempre um array (mesmo que haja apenas uma correspondência).
+=end
+
 
 string = "A linguagem Ruby é poderosa e versátil. Ruby é divertida e fácil de aprender."
 pattern = /Ruby/
