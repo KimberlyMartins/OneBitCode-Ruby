7343d63d9e320f176d3f15e4176259f8dba0077f
bowie <kiiim.martins@gmail.com>
2026-05-13 11:11:13 -0300

    Update 7-numbers.rb

--- One bit code/5-RubyAvançado/7-numbers.rb
+++ One bit code/5-RubyAvançado/7-numbers.rb
@@ -13,3 +13,10 @@ numeros_decimais = string.scan(/-?\d+\.\d+/)
 
 puts "Números inteiros encontrados: #{numeros_inteiros.join(',')}" # Números inteiros encontrados: 13,19,99
 puts "Números decimais encontrados: #{numeros_decimais.join(',')}" # Números decimais encontrados: 19.99
+
+# EXTRAIR E SOMAR NÚMEROS INTEIROS COM SCAN
+string2 = "Tenho 3 maçãs, 2 laranjas e 5 bananas em minha cesta"
+numbers = string2.scan(/\d+/).map(&:to_i)
+sum = numbers.sum
+puts "Os números encontrados são: #{numbers.join(',')}" #Os números encontrados são: 3,2,5
+puts "A soma de todos os números é: #{sum}" #A soma de todos os números é: 10
