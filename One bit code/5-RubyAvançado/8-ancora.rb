dc0a837ec102e3cc85e75a7065da556d7ef5b00b
bowie <kiiim.martins@gmail.com>
2026-05-14 11:42:25 -0300

    Update 8-ancora.rb

--- One bit code/5-RubyAvançado/8-ancora.rb
+++ One bit code/5-RubyAvançado/8-ancora.rb
@@ -15,3 +15,19 @@ A Olá, OBC! não corresponde ao padrão
 A Ruby é divertido, Olá mundo! não corresponde ao padrão
 =end
 
+#Verificação de string que começa com Ruby e termina com 3.2
+
+strings2 = ["Ruby-3.2", "Ruby-2.0", "Ruby-3.0 é poderoso"]
+strings2.each do |string2|
+    if string2 =~ /^Ruby.*3\.2$/
+        puts "A #{string2} corresponde ao padrão"
+    else
+        puts "A #{string2} não corresponde ao padrão"
+    end
+end
+
+=begin
+A Ruby-3.2 corresponde ao padrão
+A Ruby-2.0 não corresponde ao padrão
+A Ruby-3.0 é poderoso não corresponde ao padrão
+=end
