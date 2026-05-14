e09e97bd38f295033aac4b19b68b13988b1ea5ff
bowie <kiiim.martins@gmail.com>
2026-05-14 11:34:48 -0300

    Update 8-ancora.rb

--- One bit code/5-RubyAvançado/8-ancora.rb
+++ One bit code/5-RubyAvançado/8-ancora.rb
@@ -1,10 +1,12 @@
+# verificar se começa com olá e termina com mundo
 strings = ["Olá Mundo!", "Olá OBC!", "Ruby é divertido, Olá mundo!"]
 
 strings.each do |srting|
-    if srting =~/\AOlá.*mundo\z/
+    if srting =~/\AOlá.*mundo\z/  
         puts "A #{srting} corresponde ao padrão"
     else
         puts "A #{srting} não corresponde ao padrão"
     end
 end
+
 
