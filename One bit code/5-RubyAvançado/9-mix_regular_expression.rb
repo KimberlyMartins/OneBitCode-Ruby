9c499012e6dece4a90e01c434aded721b1ccd120
bowie <kiiim.martins@gmail.com>
2026-05-14 12:16:12 -0300

    Update 9-mix_regular_expression.rb

--- One bit code/5-RubyAvançado/9-mix_regular_expression.rb
+++ One bit code/5-RubyAvançado/9-mix_regular_expression.rb
@@ -1,14 +1,17 @@
 =begin
-Ainda usando expressões regulare, podemos fazer mais algumas ações como por exemplo excluir caracteres da busca
+Ainda usando expressões regulare, podemos fazer mais algumas ações como por exemplo excluir caracteres da busca, substituir valores da busca
 =end
 
 # Excluir caracteres da busca usando expressões regulares
 strings = ["apple", "cd", "cherry", "dt", "grape", "fig"]
 
 strings.each do |string|
-  if string =~ /^[^aeiou]+$/
+  if string =~ /^[^aeiou]+$/ # verificar ocorrências de vogais na string
     puts "A #{string} contem apenas consoantes"
   else
     puts "A #{string} contém vogais ou outros caracteres"
   end
 end
+
+# Substituição de informações da string sub e gsub
+string2 = "A linguagem Ruby é fácil de aprender e Ruby é divertida."
