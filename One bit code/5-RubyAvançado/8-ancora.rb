263a54ae47fdf75bd009d82743045e2f9556f486
bowie <kiiim.martins@gmail.com>
2026-05-14 11:43:33 -0300

    Update 8-ancora.rb

--- One bit code/5-RubyAvançado/8-ancora.rb
+++ One bit code/5-RubyAvançado/8-ancora.rb
@@ -1,3 +1,19 @@
+=begin
+
+Seguimos a mesma referencia dos reg ex
+\\d: Corresponde a qualquer dígito de 0 a 9.
+\\D: Corresponde a qualquer caractere que não seja um dígito.
+\\w: Corresponde a qualquer caractere alfanumérico (letras, dígitos e sublinhados).
+\\W: Corresponde a qualquer caractere que não seja alfanumérico.
+\\s: Corresponde a qualquer caractere de espaço em branco, como espaço, tabulação, nova linha, entre outros.
+\\S: Corresponde a qualquer caractere que não seja espaço em branco.
+\\b: Corresponde a uma fronteira de palavra.
+\\B: Corresponde a qualquer posição que não seja uma fronteira de palavra.
+\\A: Corresponde ao início da string.
+\\Z: Corresponde ao final da string, ou ao final da linha se a opção multiline estiver habilitada.
+=end
+
+
 # verificar se começa com olá e termina com mundo
 strings = ["Olá, mundo!", "Olá, OBC!", "Ruby é divertido, Olá mundo!"]
 
