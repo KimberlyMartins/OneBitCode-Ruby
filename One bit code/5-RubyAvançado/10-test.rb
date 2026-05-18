d6e2948c68dad2abf171abf9de5f48c396518025
bowie <kiiim.martins@gmail.com>
2026-05-18 11:14:33 -0300

    Update 10-test.rb

--- One bit code/5-RubyAvançado/10-test.rb
+++ One bit code/5-RubyAvançado/10-test.rb
@@ -6,12 +6,24 @@ def sum(a,b)
   a + b
 end
 
+def palindromo?(palavra)
+  palavra.downcase == palavra.downcase.reverse
+end
+
 class TestMathematics < Minitest::Test
   def test_sum_method
     assert_equal(5, sum(3,2))
   end
    def test_sum_method_again
     assert_equal(7, sum(4,2))
+  end
+end
+
+class TestPalindromo < Minitest::Test
+  def test_palindromo
+    assert_equal(true, palindromo?('ovo'))
+    assert_equal(true, palindromo?('Ame a ema'))
+    assert_equal(false, palindromo?('Olá)'))
   end
 end
 
@@ -70,4 +82,20 @@ Expected: 7
   Actual: 6
 
 2 runs, 2 assertions, 1 failures, 0 errors, 0 skips
+
+COM PALINDROMO RETORNA 
+Run options: --seed 42028
+
+# Running:
+
+.F.
+
+Finished in 0.003477s, 862.9369 runs/s, 1438.2281 assertions/s.
+
+  1) Failure:
+TestMathematics#test_sum_method_again [./10-test.rb:18]:
+Expected: 7
+  Actual: 6
+
+3 runs, 5 assertions, 1 failures, 0 errors, 0 skips
 =end
