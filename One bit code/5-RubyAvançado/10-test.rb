871c5fecda9b20fb853985daa65954538fe69992
bowie <kiiim.martins@gmail.com>
2026-05-18 11:08:32 -0300

    Create 10-test.rb

---
+++ One bit code/5-RubyAvançado/10-test.rb
@@ -0,0 +1,73 @@
+
+
+require "minitest/autorun"
+
+def sum(a,b)
+  a + b
+end
+
+class TestMathematics < Minitest::Test
+  def test_sum_method
+    assert_equal(5, sum(3,2))
+  end
+   def test_sum_method_again
+    assert_equal(7, sum(4,2))
+  end
+end
+
+=begin
+require "minitest/autorun"
+
+def sum(a,b)
+  a + b
+end
+
+class TestMathematics < Minitest::Test
+  def test_sum_method
+    assert_equal(5, sum(3,2))
+  end
+end
+Run options: --seed 21919
+
+# Running:
+
+.
+
+Finished in 0.002405s, 415.7658 runs/s, 415.7658 assertions/s.
+
+1 runs, 1 assertions, 0 failures, 0 errors, 0 skips
+
+
+Teste com a falha 
+
+require "minitest/autorun"
+
+def sum(a,b)
+  a + b
+end
+
+class TestMathematics < Minitest::Test
+  def test_sum_method
+    assert_equal(5, sum(3,2))
+  end
+   def test_sum_method_again
+    assert_equal(7, sum(4,2))
+  end
+end
+
+ RETORNA 
+Run options: --seed 976
+
+# Running:
+
+F.
+
+Finished in 0.002431s, 822.6729 runs/s, 822.6729 assertions/s.
+
+  1) Failure:
+TestMathematics#test_sum_method_again [./10-test.rb:14]:
+Expected: 7
+  Actual: 6
+
+2 runs, 2 assertions, 1 failures, 0 errors, 0 skips
+=end
