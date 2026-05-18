54be2dbfe639f8083d69bcdb65592175ab101a7b
bowie <kiiim.martins@gmail.com>
2026-05-18 10:54:47 -0300

    Update exer2.rb

--- One bit code/5-RubyAvançado/exer2.rb
+++ One bit code/5-RubyAvançado/exer2.rb
@@ -1,3 +1,25 @@
 =begin
 ​Crie um programa que solicite ao usuário uma data no formato "dd/mm/aaaa" e verifique se a entrada corresponde a este formato. Se sim, o programa deve imprimir a data no formato "aaaa-mm-dd". Caso contrário, deve exibir uma mensagem de erro.
 =end
+
+def verificar_e_converter_data(string)
+  if string =~/\A(\d{2})\/(\d{2})\/(\d{4})\z/
+    dia, mes, ano = string.scan(/\d+/)
+    nova_data = "#{ano}- #{mes}- #{dia}"
+    puts "A nova data é #{nova_data}"
+  else
+    puts "A data inserida não está no formato correto 'dd/mm/yyy'"
+  end
+end
+
+#Solicitar ao usuario a data 
+puts "Insira uma data n formato dd/mm/yyyy"
+data = gets.chomp
+
+verificar_e_converter_data(data)
+
+=begin
+Insira uma data n formato dd/mm/yyyy
+07/08/1996
+A nova data é 1996- 08- 07
+=end
