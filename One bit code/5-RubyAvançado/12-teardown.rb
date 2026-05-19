0568957af73b4ce7dff11624900527a07652dc8c
bowie <kiiim.martins@gmail.com>
2026-05-19 11:41:54 -0300

    Update 12-teardown.rb

--- One bit code/5-RubyAvançado/12-teardown.rb
+++ One bit code/5-RubyAvançado/12-teardown.rb
@@ -18,3 +18,45 @@ Assim, cada teste começa com um banco "limpo" e termina liberando recursos.
 =end
 
 require 'minitest/autorun'
+
+class ShoppingCart
+  def initialize
+    @itens = {}
+  end
+
+  def add_item(product, quantity)
+    @items[product] = quantity
+  end
+
+  def remove_item(product)
+    @items.delete(product)
+  end
+
+  def item_count
+    @items.values.sum
+  end
+
+  def total_price
+    products_prices = {"Headphones" => 30, "Book" => 10}
+    @items.sum {|product, quantity| quantity * products_prices[product]}
+  end
+
+  def clear
+    @items = {}
+  end
+end
+
+class TestShoppingCart < Minitest::Test
+  def setup
+    @cart = ShoppingCart.new
+  end
+
+  def teardown
+    @cart.clear
+  end
+
+  def test_add_item
+    @cart.add_item("Laptop", 1)
+    assert_equal(1, @cart.item_count, "Falha ao adicionar a o carrinho")
+  end
+end
