f4ab07e4fe0af824821de3aa3dd2bc91233c7514
bowie <kiiim.martins@gmail.com>
2026-05-19 14:28:40 -0300

    create exer 3

---
+++ One bit code/5-RubyAvançado/exer3.rb
@@ -0,0 +1,17 @@
+=begin
+​Exercício: Calculadora Simples com Minitest
+
+Descrição:
+
+Vamos criar uma calculadora simples em Ruby e escrever testes para garantir que as operações
+
+matemáticas básicas estejam corretas. Use o Minitest para criar testes para as funções de adição,
+
+subtração, multiplicação e divisão.
+
+Especificações:
+
+Crie uma classe chamada Calculator que tenha métodos para adição, subtração, multiplicação e divisão.
+Escreva testes para cada um desses métodos usando o Minitest.
+Certifique-se de testar diferentes cenários, incluindo números positivos, negativos e a divisão por zero.
+=end
