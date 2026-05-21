2160c12927cf0ff95e28b761044330a9b13fc974
bowie <kiiim.martins@gmail.com>
2026-05-21 14:25:04 -0300

    add elif

--- exer1.rb
+++ exer1.rb
@@ -107,10 +107,14 @@ def fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5/9
 
 def main():   
-   print("Bem-vindo ao conversor de temperaturas!")
+   choice = input("Digite 'C' para converter Celsius para Fahrenheit ou 'F' para converter Fahrenheit para Celsius: ").upper()
+
+   if choice == 'C':
+    print("Bem-vindo ao conversor de temperaturas!")
     temp_c = float(input("Digite a temperatura em Celsius: "))
     temp_f = celsius_to_fahrenheit(temp_c)
     print(f"{temp_c}°C é igual a {temp_f}°F")
+  elif choice == 'F':
     temp_f = float(input("Digite a temperatura em Fahrenheit: "))
     temp_c = fahrenheit_to_celsius(temp_f)
     print(f"{temp_f}°F é igual a {temp_c}°C")
