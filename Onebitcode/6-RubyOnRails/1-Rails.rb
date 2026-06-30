ec865562e993806d4e53e808b39c91f128f3a3a1
bowie <kiiim.martins@gmail.com>
2026-06-30 13:10:55 -0300

    Create 1-Rails.rb
    rails description

---
+++ Onebitcode/6-RubyOnRails/1-Rails.rb
@@ -0,0 +1,42 @@
+                             FRAMEWORK
+Um framework é um conjunto de códigos, regras e ferramentas que serve como base para desenvolver aplicações, evitando que o programador precise criar tudo do zero.
+
+                         RUBY ON RAILS (RAILS)
+O Ruby on Rails, também chamado apenas de Rails, é um framework de desenvolvimento web criado para a linguagem de programação Ruby. Ele facilita a criação de sites e sistemas, fornecendo uma estrutura pronta e várias ferramentas utilizadas para desenvolver sites, sistemas web, APIs, E-commerces, plataformas de gestão ,redes sociais e outros sistemas online.
+É um framework web open source escrito em Ruby, que utiliza o padrão MVC e os princípios DRY e Convention over Configuration. Ele oferece bibliotecas como Active Record, Action Pack e Action Mailer, tornando o desenvolvimento de aplicações web mais rápido, organizado e seguro.
+
+Principais características
+Produtividade: permite desenvolver mais rápido.
+Código organizado: segue um padrão de organização.
+Reaproveitamento de código: evita escrever o mesmo código várias vezes.
+Segurança: possui recursos de segurança já integrados.
+
+          Convenção sobre Configuração (Convention over Configuration):
+ é a ideia de usar padrões prontos para reduzir configurações manuais, tornando o desenvolvimento mais rápido e simples evitando configurações desnecessárias.
+
+
+                        DRY (Dont Repeat Yourself)
+O princípio DRY vem da expressão Don’t Repeat Yourself (“não se repita”). Ele é muito usado no Ruby on Rails e em outros frameworks para evitar que o programador escreva o mesmo código várias vezes.
+
+                            Arquitetura MVC
+O Rails utiliza o padrão MVC (Model-View-Controller).
+MODEL: gerencia os dados e o banco de dados. Exemplo: informações de usuários, produtos, pedidos.
+VIEW: exibe as informações para o usuário (interface). Exemplo: páginas HTML, formulários, botões.
+CONTROLLER: recebe as requisições do usuário, processa as informações e conecta o Model à View. Exemplo: clicar em um botão, e decide o que mostrar ou salvar.
+Banco de dados
+
+O Rails utiliza o Active Record, que facilita a comunicação entre o sistema e o banco de dados, permitindo manipular dados usando Ruby em vez de escrever SQL o tempo todo.
+Vantagens
+Desenvolvimento rápido.
+Código limpo e organizado.
+Grande quantidade de bibliotecas (gems).
+Comunidade ativa.
+Fácil manutenção.
+Resumindo
+Ruby é a linguagem de programação.
+Ruby on Rails é o framework que utiliza Ruby para criar aplicações web de forma rápida, organizada e seguindo o padrão MVC.
+Dica para memorizar:
+Ruby = linguagem
+Rails = framework
+Ruby + Rails = desenvolvimento de aplicações web de forma rápida e organizada.
+=end
